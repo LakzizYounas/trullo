2dfce41a77e46aaa79b41b5d65557b30b4fdc1b8
younaas <lakziz_y@etna-alternance.net>
2020-07-03 19:44:23 +0200

    apply theme for button

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as GoogleSvg } from '../../assets/google_logo.svg';
 export const StyledButton = styled.button`
   display: flex;
   justify-content: center;
-  ${({google}) => google ? 'background-color: #4285f4' : 'background-color: #4CAF50'};
+  ${({google}) => google ? 'background-color: #4C8BF5' : 'background-color: #4CAF50'};
   color: white;
   border: none;
   border-radius: 4px;
@@ -22,7 +22,7 @@ export const StyledButton = styled.button`
 
 export const StyledGoogle = styled(GoogleSvg)`
   height: 30px;
-  filter: drop-shadow(1px 1px 1px black);
+  filter: drop-shadow(2px 2px 1px ${({theme}) => theme.main});
 `;
 
 export const StyledText = styled.div`
